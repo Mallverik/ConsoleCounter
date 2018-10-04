47eaae353e9db0ae592998753f6502299af42e6a
Mallverik <42370162+Mallverik@users.noreply.github.com>
2018-10-04 17:24:40 +0300

    Done

---
+++ Test/Test/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Functional
+    {
+        public event Action<string> TextCheckerEvent;
+        int counter = 0;
+
+        public void TextCheck(string UserText)
+        {
+
+            for (int i = 0; i < UserText.Length; i++)
+            {
+                if (UserText[i] == '+')
+                {
+                    counter++;
+                    if (counter % 3 == 0 && counter != 0)
+                    {
+                        TextCheckerEvent?.Invoke($"{counter} кратне числу 3");
+                    }
+
+                }
+                else if (UserText[i] == '-')
+                {
+                    counter--;
+                    if (counter % 3 == 0 && counter != 0)
+                    {
+                        TextCheckerEvent?.Invoke($"{counter} кратне числу 3");
+                    }
+                }
+
+            }
+        }
+        public void CounterShow()
+        {
+            TextCheckerEvent?.Invoke($"Counter = {counter}");
+        }
+
+      
+    }
+       
+}
+
---
+++ Test/Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+
+   
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+         
+            Functional functional = new Functional();
+            Console.WriteLine("Enter your text: ");
+            
+            functional.TextCheckerEvent += ShowMessage;
+            functional.TextCheck(Console.ReadLine());
+            functional.CounterShow();
+            Console.ReadLine();
+        }
+
+        public static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+    //Створити консольний додаток який зчитує введений текст з клавіатури, 
+    //кожного роазу коли в рядку буде "+", то потрібно щоб counter +1, якщо в рядку буде "-" то зменшувати, 
+    //зробити на основі event та додати обробник події(кожен раз коли counter - кратний 3) то виводити про це повідомлення
+}
+
